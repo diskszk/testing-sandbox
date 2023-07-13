0c5d816ad7ad4eb9e78e73860a837e81d40be1a4
diskszk <ibalucky23@gmail.com>
2023-07-13 22:47:13 +0900

    WIP: 記事: 用のコードを書く

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import { Layout } from "../components/Layout";
+import { CreatePostForm } from "../tdd-with-rhf-zod/CreatePostForm";
 
 export const HomePage: React.FC = () => {
   return (
     <Layout title="Home">
-      <div></div>
+      <div>
+        <CreatePostForm
+          handleSubmitPost={(data) => console.log(data, "が定義されています")}
+        />
+      </div>
     </Layout>
   );
 };
